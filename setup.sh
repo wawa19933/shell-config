e63797a5bc7359daf5657e3358558edfdba0d3d8
Sergio Scalpone <wawa19933@gmail.com>
2016-07-19 13:40:46 +0200

    Fixed setup.sh sed command arguments

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@
 RC_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 echo "Resources dir: ${RC_DIR}"
 
-sed -i "s/RCDIR=.*/RCDIR=$RC_DIR" ${RC_DIR}/bashrc
-sed -i "s/ZDOTDIR=.*/ZDOTDIR=$RC_DIR" ${RC_DIR}/zshrc
+sed -i "s:RCDIR=.*:RCDIR=$RC_DIR:" ${RC_DIR}/bashrc
+sed -i "s:ZDOTDIR=.*:ZDOTDIR=$RC_DIR:" ${RC_DIR}/zshrc
 
 ln -frs $RC_DIR/bashrc ${HOME}/.bashrc
 ln -frs $RC_DIR/zshrc ${HOME}/.zshrc
