6456f972bcaf784fef0071c3ec4777630e6584c9
Sergio Scalpone <wawa19933@gmail.com>
2016-09-29 05:35:35 +0200

    Adding debug messages to setup.sh

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ echo "Resources dir: ${RC_DIR}"
 sed -i "s:RCDIR=.*:RCDIR=$RC_DIR:" ${RC_DIR}/bashrc ${RC_DIR}/zshrc
 # sed -i "s:ZDOTDIR=.*:ZDOTDIR=$RC_DIR:" ${RC_DIR}/zshrc
 
+echo "Creating links in home dir..."
 ln -frs $RC_DIR/bash_profile.sh ${HOME}/.bash_profile
 ln -frs $RC_DIR/bash_logout.sh ${HOME}/.bash_logout
 ln -frs $RC_DIR/bashrc ${HOME}/.bashrc
@@ -20,7 +21,8 @@ mv progress ~/.local/bin/
 #   git clone https://github.com/powerline/powerline.git ${POWERLINE_DIR} && \
 #   ln -rsf ${POWERLINE_DIR}/scripts/* ${HOME}/.local/bin/
 
-[ -d ~/.local/powerline-shell ] || \
+[ -d ~/.local/powerline-shell ] ||\
+  echo "Installing powerline-shell" &&\
   git clone https://github.com/milkbikis/powerline-shell ~/.local/powerline-shell &&\
     cd ~/.local/powerline-shell; ~/.local/powerline-shell/install.py; cd - || cd 
 
@@ -29,6 +31,7 @@ mv progress ~/.local/bin/
 #   curl -L https://bit.ly/glances | bash
 
 [ -d ~/.vim/autoload ] || makedir -p ~/.vim/autoload && \
+  echo "Installing vim.plug..." &&\  
   wget -O ~/.vim/autoload/plug.vim \
      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim || \
     curl -o ~/.vim/autoload/plug.vim \
@@ -36,12 +39,14 @@ mv progress ~/.local/bin/
 
 
 [ -d ~/.config/nvim/autoload ] || makedir -p ~/.config/nvim/autoload && \
+  echo "Installing nvim.plug" &&\  
   wget -O ~/.config/nvim/autoload/plug.vim \
      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim || \
     curl -o ~/.config/nvim/autoload/plug.vim \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 if [[ ! -d ~/.fzf ]]; then
+  echo "Installing fzf"  
   git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
   bash ~/.fzf/install --all --no-update-rc
   ln -rsf ~/.fzf/bin/fzf-tmux ~/.local/bin/ && ln -rsf ~/.fzf/bin/fzf ~/.local/bin/ 
